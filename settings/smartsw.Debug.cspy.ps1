c4d397d2a5f6ac081c03ea0941be41581425bdf9
phatnm <phatnm@users.noreply.github.com>
2018-06-14 17:24:00 +0700

    update

--- settings/smartsw.Debug.cspy.ps1
+++ settings/smartsw.Debug.cspy.ps1
@@ -23,9 +23,9 @@ param([String]$debugfile = "");
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\EMBEDDED\stm8phat\smartsw\settings\smartsw.Debug.general.xcl" --backend -f "D:\EMBEDDED\stm8phat\smartsw\settings\smartsw.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\GITembed\smartsw\settings\smartsw.Debug.general.xcl" --backend -f "D:\GITembed\smartsw\settings\smartsw.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\EMBEDDED\stm8phat\smartsw\settings\smartsw.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\EMBEDDED\stm8phat\smartsw\settings\smartsw.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\GITembed\smartsw\settings\smartsw.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\GITembed\smartsw\settings\smartsw.Debug.driver.xcl" 
 }
