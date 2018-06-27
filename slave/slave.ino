d4305b9f222296f2332e241bb5fe15890c388554
nghiahdt <nghiahdt@ntgames.net>
2018-06-27 16:37:44 +0700

    - nrf

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,16 +1,23 @@
 #include "relay.h"
 #include "button.h"
+#include "nrf.h"
 
 void setup()
 {
-	relay_init(Relay1);
-	button_init(Button1);
+	//relay_init(Relay1);
+	//button_init(Button1);
+	nrf_init();
+	nrf_set_tx_mode();
 }
+
+unsigned char tx_buf[NRF_PAYLOAD_WIDTH] = { 'a', 'b', 'z'};
 
 void loop()
 {
-	if (button_just_pressed(Button1))
-	{
-		relay_change(Relay1);
-	}
+	//if (button_just_pressed(Button1))
+	//{
+	//	relay_change(Relay1);
+	//}
+	nrf_tx_packet(tx_buf);
+	delay(1000);
 }
