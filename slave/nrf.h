262e40dddee8b303024aea8eb6e5560e3004a105
nghiahdt <nghiahdt@ntgames.net>
2018-07-03 16:24:47 +0700

    - xxx

--- slave/nrf.h
+++ slave/nrf.h
@@ -4,10 +4,10 @@
 #define NRF_CSN_PIN			PC4
 #define NRF_SCK_PIN			PD2	
 #define NRF_MOSI_PIN		PD4
-#define NRF_MISO_PIN		PD1
+#define NRF_MISO_PIN		PC6
 
 #define NRF_PAYLOAD_LENGTH	32
-#define NRF_CHANNEL			2
+#define NRF_CHANNEL			64
 #define NRF24_CONFIG_CRC_2_BYTE
 #define NRF24_ADDR_LEN		5
 uint8_t tx_address[NRF24_ADDR_LEN] = {0xD7,0xD7,0xD7,0xD7,0xD7};
@@ -25,34 +25,39 @@ void nrf_begin()
 
 void nrf24_setupPins()
 {
-	pinMode(NRF_CE_PIN, OUTPUT);
-	pinMode(NRF_CSN_PIN, OUTPUT);
-	pinMode(NRF_SCK_PIN, OUTPUT);
-	pinMode(NRF_MOSI_PIN, OUTPUT);
+	pinMode(NRF_CE_PIN, OUTPUT_FAST);
+	pinMode(NRF_CSN_PIN, OUTPUT_FAST);
+	pinMode(NRF_SCK_PIN, OUTPUT_FAST);
+	pinMode(NRF_MOSI_PIN, OUTPUT_FAST);
 	pinMode(NRF_MISO_PIN, INPUT_PULLUP);
 }
          
 void nrf24_ce_digitalWrite(uint8_t state)
 {
 	digitalWrite(NRF_CE_PIN, state);
+	delayMicroseconds(1);
 }
 
 void nrf24_csn_digitalWrite(uint8_t state)
 {
 	digitalWrite(NRF_CSN_PIN, state);
+	delayMicroseconds(1);
 }
 
 void nrf24_sck_digitalWrite(uint8_t state)
 {
 	digitalWrite(NRF_SCK_PIN, state);
+	delayMicroseconds(1);
 }
 
 void nrf24_mosi_digitalWrite(uint8_t state)
 {
 	digitalWrite(NRF_MOSI_PIN, state);
+	delayMicroseconds(1);
 }
 
 uint8_t nrf24_miso_digitalRead()
 {
+	delayMicroseconds(1);
     return digitalRead(NRF_MISO_PIN);
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -4,21 +4,28 @@
 
 void setup()
 {
-	//relay_init(Relay1);
-	//button_init(Button1);
-	nrf_begin();
+  relay_init(Relay1);
+  //button_init(Button1);
+  nrf_begin();
 }
 
 void loop()
 {
-	//if (button_just_pressed(Button1))
-	//{
-	//	relay_change(Relay1);
-	//}
+  uint8_t data[NRF_PAYLOAD_LENGTH] = { 'f', 'u', 'c', 'k', 0};
 
-	//uint8_t data[NRF_PAYLOAD_LENGTH] = { 'a', 'b', 'c', 0};
-	//nrf24_send(data);
-	//while(nrf24_isSending());
+  //if (button_just_pressed(Button1))
+  {
+    relay_change(Relay1);
+  }
 
-	delay(1000);
+  if (nrf24_getStatus() == 255)
+  {
+    relay_on(Relay1);
+  }
+
+  nrf24_send(data);
+  while(nrf24_isSending());
+
+  delay(100);
 }
+
